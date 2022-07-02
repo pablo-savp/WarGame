220f0478af1091885a7f57e8f91bbefcc922911c
santander_p <78176446+pablo-savp@users.noreply.github.com>
2022-07-02 23:01:38 +0000

    Attributes changed

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
-            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':14}
 
 class Card:
   
   def __init__(self,type,rank):
-    self.tipo=type
-    self.rango=rank
+    self.type=type
+    self.rank=rank
     self.value=values[rank]
     
   def __str__(self):
