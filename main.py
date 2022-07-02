02e1500f397b5953dd0c3abd5528bce02a3579c1
santander_p <78176446+pablo-savp@users.noreply.github.com>
2022-07-02 23:04:28 +0000

    Minor fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
-            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':14}
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
   
@@ -70,12 +70,12 @@ while game_on:
   print(f"Round {roundNumber}")
 
   if len(player_one.all_cards) ==0:
-    print("Player one is out of cards! Player Two Wins")
+    print("Player One is out of cards! Player Two Wins")
     game_on = False
     break
 
   if len(player_two.all_cards) ==0:
-    print("Player two is out of cards! Player One Wins")
+    print("Player Two is out of cards! Player One Wins")
     game_on = False
     break
 
